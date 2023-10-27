a1b39e1ef937fcacc372786c7473e74bbb2da346
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-10-27 21:02:05 +0900

    Feat : GameManager 기초 구현

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    public int Gold = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;   
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
