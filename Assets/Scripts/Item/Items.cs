4c5a21449a18a4a5714650609756207afd70283f
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-10-27 16:27:12 +0900

    Feat : Items Script 생성 및 GameManager 생성

---
+++ Assets/Scripts/Item/Items.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * 2; // 2 = speed
+    }
+
+    protected virtual void ItemEffect(GameObject other)
+    {
+
+    }
+}
