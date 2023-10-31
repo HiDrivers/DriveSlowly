6b49e5bd8f7abef240486ffc3bddfb8ba395ee11
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-10-31 20:16:20 +0900

    [Fix]TalkManager.cs 수정
    Start부분에서 바로 대화를 작성 했던 것을 다른 씬에서도 확장해서 사용 할 수 있도록 수정.

--- Assets/HYJ_Test/Scripts/TalkManager.cs
+++ Assets/HYJ_Test/Scripts/TalkManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TalkManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class TalkManager : MonoBehaviour
     private bool isTyping = false;
     private Queue<Dialogue> dialogueQueue = new Queue<Dialogue>();
 
+
     public struct Dialogue
     {
         public string speaker;
@@ -20,16 +22,49 @@ public class TalkManager : MonoBehaviour
 
     void Start()
     {
-        dialogueQueue.Enqueue(new Dialogue { speaker = "Ham", text = "Hello, how are you?" });
-        dialogueQueue.Enqueue(new Dialogue { speaker = "Young", text = "I'm good, thanks!" });
-        dialogueQueue.Enqueue(new Dialogue { speaker = "Joo", text = "OMG i need a HanguelFont " });
-        dialogueQueue.Enqueue(new Dialogue { speaker = "haha", text = "This is Test" });
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+   
+        if (currentSceneName == "HYJ_TestScene")
+        {
+            CutScene1();
+        }
+        else if (currentSceneName == "HYJ_TestScene2")
+        {
+            CutScene2();
+        }
 
         ShowNextDialogue();
     }
 
+    void CutScene1()
+    {
+        dialogueQueue.Clear(); // 이전 대사를 비우고 새로운 대사를 설정
+
+        dialogueQueue.Enqueue(new Dialogue { speaker = "알람시계", text = "RRRRRRRR(일.어.나)" });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "주인공", text = "Zzzzz..쿠-울..." });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "주인공", text = "음.. 졸려.. 지금 몇시지?" });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "주인공", text = "!!!! 8시 40분?!\n 안돼 9시까지 출근 해야 하는데 지각이다" });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "주인공", text = "이번에도 지각하면 정직원 전환은 ...진짜 끝이야" });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "주인공", text = "..." });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "주인공", text = "..." });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "주인공", text = "벌금이 문제가 아니야... 밟아 OOO 제발 정직원 가즈아" });
+
+
+    }
+
+    void CutScene2()
+    {
+        dialogueQueue.Clear();
+
+        dialogueQueue.Enqueue(new Dialogue { speaker = "Joo", text = "OMG i need a HanguelFont " });
+        dialogueQueue.Enqueue(new Dialogue { speaker = "haha", text = "This is Test" });
+    }
+
     void ShowNextDialogue()
     {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
         if (dialogueQueue.Count > 0)
         {
             Dialogue dialogue = dialogueQueue.Dequeue();
@@ -37,7 +72,14 @@ public class TalkManager : MonoBehaviour
         }
         else
         {
-            // 대화 종료 또는 다른 작업 수행
+            if (currentSceneName == "HYJ_TestScene")
+            {
+                StartCoroutine(LoadSceneAfterDelay("HYJ_TestScene2", 2.0f));
+            } 
+            else if (currentSceneName == "HYJ_TestScene2")
+            {
+                StartCoroutine(LoadSceneAfterDelay("SampleScene", 2.0f));
+            }
         }
     }
 
@@ -56,10 +98,7 @@ public class TalkManager : MonoBehaviour
 
         isTyping = false;
 
-        while (!Input.GetMouseButtonDown(0))
-        {
-            yield return null;
-        }
+        yield return new WaitForSeconds(0.5f);
 
         ShowNextDialogue();
     }
@@ -67,5 +106,13 @@ public class TalkManager : MonoBehaviour
     IEnumerator EnterText(string speaker, string text)
     {
         yield return StartCoroutine(NormalChat(speaker, text));
+    }
+
+    IEnumerator LoadSceneAfterDelay(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // 지정된 시간이 지난 후 다음 씬을 로드
+        SceneManager.LoadScene(sceneName);
     }
 }
