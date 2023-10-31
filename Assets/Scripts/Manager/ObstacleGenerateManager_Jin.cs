09887ca2577aad6dce641954c673c355aed52ebb
GYALLERHORN <kjins1998@gmail.com>
2023-10-31 10:46:16 +0900

    Update : Singleton 상위 스크립트 적용

--- Assets/Scripts/Manager/ObstacleGenerateManager_Jin.cs
+++ Assets/Scripts/Manager/ObstacleGenerateManager_Jin.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleGenerateManager_Jin : MonoBehaviour
+public class ObstacleGenerateManager_Jin : Singleton<ObstacleGenerateManager_Jin>
 {
     // 4개의 차선
     // 차선에 맞춰 생성되는 장애물(자동차)
     // 단일 장애물만 생성되는가?
-    // 장애물 이외의 생성 오브젝트는 없는가? 있다면 이 메니저로 같이 관리할 수 있는가?
+    // 장애물 이외의 생성 오브젝트는 없는가? 있다면 이 매니저로 같이 관리할 수 있는가?
     // 
 
     private List<Transform> prefabGenPos;
-
 
 }
