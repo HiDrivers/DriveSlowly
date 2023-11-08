feb4a1394b34898724fe737c6225895b0c5e077b
kimyeonwoo1234 <141593594+kimyeonwoo1234@users.noreply.github.com>
2023-11-08 11:28:59 +0900

    feat) ui manager
    feat) ui manager

---
+++ Assets/Scripts/temp/Scene Loader.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // 첫 번째 버튼이 클릭될 때 이동할 씬 이름
+    public string sceneName1 = "Select SceneA";
+
+    // 두 번째 버튼이 클릭될 때 이동할 씬 이름
+    public string sceneName2 = "Select SceneB";
+
+    public string sceneName3 = "LobbyScene";
+
+    public string sceneName4 = "MainScene";
+
+    public string sceneName5 = "SettingScene";
+    // 첫 번째 버튼 클릭 시 호출될 함수
+    public void LoadScene1OnClick()
+    {
+        SceneManager.LoadScene(sceneName1);
+    }
+
+    // 두 번째 버튼 클릭 시 호출될 함수
+    public void LoadScene2OnClick()
+    {
+        SceneManager.LoadScene(sceneName2);
+    }
+
+    public void LoadScene3OnClick()
+    {
+        SceneManager.LoadScene(sceneName3);
+    }
+
+    public void LoadScene4OnClick()
+    {
+        SceneManager.LoadScene(sceneName4);
+    }
+
+    public void LoadScene5OnClick()
+    {
+        SceneManager.LoadScene(sceneName5);
+    }
+}
+
+
