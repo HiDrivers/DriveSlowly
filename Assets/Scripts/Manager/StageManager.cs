0245117eb560e0ed997199d7fd795b9a66d1ac38
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-11-09 15:52:37 +0900

    [Feat] StageManager 싱글톤 해제 및 Stage 1, 2, 3 별개의 Manager로 관리도되록 구현

--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -4,39 +4,36 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StageManager : Singleton<StageManager>
+public class StageManager : MonoBehaviour
 {
     public GameObject spawnPoint;
 
-    [SerializeField] private GameObject coinPrefab;
-    [SerializeField] private GameObject boosterPrefab;
+    [SerializeField] protected GameObject coinPrefab;
+    [SerializeField] protected GameObject player;
+    [SerializeField] protected GameObject ControlUI;
 
     public float clearTime = 120;
     public float currentTime;
 
-    public float maxHp;
-    public float curHp;
+    protected float itemTimer = 0;
+    protected float obstacleTimer = 0;
 
-    private float itemTimer = 0;
-    private float obstacleTimer = 0;
-
-    [SerializeField] private float itemSpawnCool;
-    private float obstacleSpawnCool = 0;
+    [SerializeField] protected float itemSpawnCool;
+    protected float obstacleSpawnCool = 0;
 
     public Slider progress;
     public Slider durability;
     public TMP_Text gold;
 
-    private bool gameClear = false;
+    protected bool gameClear = false;
 
-    private void Start()
+    protected void Start()
     {
         currentTime = 0;
         gold.text = $"{GameManager.Instance.gold} G";
-        curHp = maxHp;
     }
 
-    private void Update()
+    protected void Update()
     {
         if (!gameClear)
         {
@@ -50,48 +47,12 @@ public class StageManager : Singleton<StageManager>
             }
 
             progress.value = (float) currentTime / clearTime;
-            durability.value = curHp / maxHp;
+            durability.value = player.GetComponent<HealthSystem>().curHp / player.GetComponent<HealthSystem>().maxHp;
             gold.text = $"{GameManager.Instance.gold} G";
-            Stage1();
             if (currentTime >= clearTime)
             {
                 gameClear = true;
             }
         }
-    }
-
-    public void Stage1()
-    {
-        // 酒捞袍 积己 包府
-        if (itemTimer > itemSpawnCool)
-        {
-            int index = Random.Range(0, 4);
-            int itemIdx = Random.Range(0, 2);
-            if (itemIdx == 0)
-            {
-                Instantiate(coinPrefab, spawnPoint.transform.GetChild(0).gameObject.transform.GetChild(index).gameObject.transform);
-            }
-            else
-            {
-                Instantiate(boosterPrefab, spawnPoint.transform.GetChild(0).gameObject.transform.GetChild(index).gameObject.transform);
-            }
-            itemTimer = 0;
-        }
-
-        // 厘局拱 积己 包府
-        if (obstacleTimer > obstacleSpawnCool)
-        {
-            ObstacleGenerateManager_Jin.Instance.CreateObstacle();
-            obstacleTimer = 0;
-            obstacleSpawnCool = Random.Range(2.0f, 4.0f);
-        }
-    }
-
-    public void Stage1Clear()
-    {
-        // 备泅 抗沥
-        Debug.Log("GameClear");
-        // GameClear UI
-        // 单捞磐 历厘
     }
 }
