875382e68bd8d24c2f1f67e36cbe92aaa018b13d
GYALLERHORN <kjins1998@gmail.com>
2023-11-24 10:30:11 +0900

    Feat : CarSelectUI 작성 중

---
+++ Assets/Scripts/UI/CarSelectControl/CarSelectController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSelectController : MonoBehaviour
+{
+    //[SerializeField] private int carNumber;
+    //[SerializeField] private int carPrice;
+
+    [SerializeField] private List<GameObject> carSlots;
+
+}
