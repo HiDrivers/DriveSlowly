91cbe8dc6a5d8617009d161c8ecd5f80b1ace07b
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-11-27 20:35:12 +0900

    Refactor : 아이템 코드 리팩토링

--- Assets/Scripts/Item/Pillow.cs
+++ Assets/Scripts/Item/Pillow.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Pillow : Items
 {
     private SoundManager soundManager; // SoundManager의 참조
+    private GameManager gameManager;
 
     private void Start()
     {
         soundManager = SoundManager.Instance;
+        gameManager = GameManager.Instance;
     }
 
     public override void ItemEffect(GameObject player)
     {
-        GameManager.Instance.sleepTimer = 15.0f;
-        GameManager.Instance.isSleep = true;
+        gameManager.sleepTimer = 15.0f;
+        gameManager.isSleep = true;
         soundManager.PlayItemSound("Pillow");
 
         // 아이템의 다른 효과들...
