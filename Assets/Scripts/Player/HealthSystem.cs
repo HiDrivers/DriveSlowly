2033923d4cc7b53afc71defce0834da22545c877
GYALLERHORN <kjins1998@gmail.com>
2023-11-29 11:18:36 +0900

    Update : Player오브젝트 서열 수정_2

--- Assets/Scripts/Player/HealthSystem.cs
+++ Assets/Scripts/Player/HealthSystem.cs
@@ -1,21 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
+    // 플레이어의 현재 상태를 다루는 스크립트, 플레이어 스프라이트 반영도 일단 이 스크립트에서 작성합니다.
+
+    [SerializeField] public GameObject carPrefab;
+    [SerializeField] private GameObject tempcarPrefab;
+
     public float curHp;
     public float maxHp;
+
+    //private void Awake()
+    //{
+    //    carPrefab = tempcarPrefab;
+    //}
 
     private void Start()
     {
         curHp = maxHp;
     }
-    private void OnCollisionStay2D(Collision2D collision)
+
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Obstacle"))
+    //    {
+    //        curHp -= Time.deltaTime;
+    //    }
+    //}
+
+    internal void UpdateCurHp()
     {
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            curHp -= Time.deltaTime;
-        }
+        curHp -= Time.deltaTime;
     }
 }
