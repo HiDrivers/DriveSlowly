6240ba6bcd16bd616e4b98a58d4516d58190cb67
GYALLERHORN <kjins1998@gmail.com>
2023-12-06 20:12:52 +0900

    fix : DamageIndicator 주석처리
    오류 발생, 주석처리

--- Assets/Scripts/Player/PlayerCollisionDetector.cs
+++ Assets/Scripts/Player/PlayerCollisionDetector.cs
@@ -15,7 +15,7 @@ public class PlayerCollisionDetector : MonoBehaviour
     private void Start()
     {
         playerHealth = GetComponentInParent<HealthSystem>();
-        damagedPanel = damagedIndicator.GetComponent<Image>();
+        //damagedPanel = damagedIndicator.GetComponent<Image>();
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
@@ -23,23 +23,23 @@ public class PlayerCollisionDetector : MonoBehaviour
         {
             playerHealth.UpdateCurHp();
 
-            StartCoroutine(GetDamageIndicate());
+            //StartCoroutine(GetDamageIndicate());
         }
     }
 
-    IEnumerator GetDamageIndicate()
-    {
-        float startAlpha = 0.5f;
-        float a = startAlpha;
-        damagedPanel.enabled = true;
+    //IEnumerator GetDamageIndicate()
+    //{
+    //    float startAlpha = 0.5f;
+    //    float a = startAlpha;
+    //    damagedPanel.enabled = true;
 
-        while (a > 0f)
-        {
-            a -= (startAlpha / damageBlinkInterval) * Time.deltaTime;
-            damagedPanel.color = new Color(0.8f, 0f, 0f, a);
-            yield return null;
-        }
+    //    while (a > 0f)
+    //    {
+    //        a -= (startAlpha / damageBlinkInterval) * Time.deltaTime;
+    //        damagedPanel.color = new Color(0.8f, 0f, 0f, a);
+    //        yield return null;
+    //    }
 
-        damagedPanel.enabled = false;
-    }
+    //    damagedPanel.enabled = false;
+    //}
 }
