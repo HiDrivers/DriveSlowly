83cc4ebba953a57e0c03f31274844bf3fee61a9a
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-12-12 03:59:05 +0900

    [Fix] SettingUIwithReset 생성 및 게임 초기화 기능 구현

---
+++ Assets/Scripts/Datas/DataReset.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DataReset : MonoBehaviour
+{
+    [SerializeField] private GameObject ResetUI;
+
+    public void ResetUIControl()
+    {
+        ResetUI.SetActive(!ResetUI.activeSelf);
+    }
+    public void GameReset()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
